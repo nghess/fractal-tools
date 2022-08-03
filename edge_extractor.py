85f0b78b17c0ff09badf1ad27c51ba189f084f21
nghess <nhess@uoregon.edu>
2022-08-02 17:48:53 -0700

    edge extractor

--- edge_extractor.py
+++ edge_extractor.py
@@ -1,18 +1,16 @@
-import numpy as np
+import os
 import cv2
 from os import listdir
 from os.path import isfile, join
 
-mypath = '/solid/'
+os.chdir('D:/ImageJ/tutorial')
+mypath = 'D:/ImageJ/tutorial/solid/'
 
 # Import Images
-filenames = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-#images = []
-edges = []
+filenames = sorted([f for f in listdir(mypath) if isfile(join(mypath, f))])
 
-for i in len(filenames):
-    image = (cv2.imread(mypath + filenames[i], 1))
-    canny = cv2.Canny(image, 100, 200)
-    canny = cv2.bitwise_not(canny)
-    cv2.imwrite(f'/edges/', {filenames[i]}, canny)
-
+for i in range(len(filenames)):
+    image = cv2.imread(mypath + filenames[i], 0)  # Load grayscale
+    canny = cv2.Canny(image, 100, 200)  # Edge Extract
+    canny = cv2.bitwise_not(canny)  # Invert
+    cv2.imwrite(f'D:/ImageJ/tutorial/edges/{filenames[i]}', canny)
